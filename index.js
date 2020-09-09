bbdd42bd6e4df9883b9fc7052796a888e59b3d2b
Andrew Poppenberg <andrewpoppenberg@Andrews-MacBook-Pro.local>
2020-09-09 14:35:57 -0500

    done with the project starting the stretch

--- index.js
+++ index.js
@@ -110,11 +110,17 @@ Hint: You can use .splice() for this
 
 */
 
-function removeFlavorByName(/*code here*/){
+function removeFlavorByName(arr,flavor){
 
-    /*code here*/
+        b = arr.indexOf(flavor);
+        arr.splice(b,1);
+        console.log(arr);       
+    
+ }
+   removeFlavorByName(originalFlavors,'Vanilla');     
+    
 
-}
+
 
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
@@ -125,12 +131,14 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+function copy(arr1,arr2){
 
-    /*code here*/
+    arr2 = [...arr1]
+    console.log(arr1);
+    console.log(arr2);
 
 }
-
+copy(originalFlavors,'notOriginalFlavors');
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
@@ -146,12 +154,17 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(arr,string){
+    let b = [];
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i].includes(string)){
+          b.push(arr[i]);  
+        }
+        
+    }
+    console.log(b);
 }
-
+filterByWord(originalFlavors,'Chocolate');
 
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
